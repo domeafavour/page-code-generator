6728e69aa5d9aa8468b07c68a972ac462bb1a1cc
zhanghaojie <1455123087@qq.com>
2024-04-15 19:03:12 +0800

    Add ensureArray function to array.ts and corresponding tests

--- src/utils/array.test.ts
+++ src/utils/array.test.ts
@@ -1,7 +1,23 @@
-import { arrayPush } from './array';
+import { arrayPush, ensureArray } from './array';
 
 describe('arrayPush', () => {
   it('should be [1, 2, 3]', () => {
     expect(arrayPush([1, 2], 3)).toEqual([1, 2, 3]);
+  });
+});
+
+describe('ensureArray', () => {
+  it('should be array itself', () => {
+    const array = [1, 2, 3];
+    expect(ensureArray(array)).toBe(array);
+  });
+
+  it('should be a new empty array if null', () => {
+    expect(ensureArray(null)).toEqual([]);
+  });
+
+  it('should be a new empty array if undefined', () => {
+    expect(ensureArray()).toEqual([]);
+    expect(ensureArray(undefined)).toEqual([]);
   });
 });
--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,3 +1,7 @@
 export function arrayPush<T>(array: T[], item: T) {
   return [...array, item];
 }
+
+export function ensureArray<T>(array: T[] | null | undefined | void): T[] {
+  return array || [];
+}
