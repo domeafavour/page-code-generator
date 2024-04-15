8c71b00009293aba06f0ab50b0d9843a080d9f1b
zhanghaojie <1455123087@qq.com>
2024-04-15 10:25:49 +0800

    Disable deploy automatically while branch main is pushed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // for github pages
-  base: '/vite-react-tw-jest-template/',
+  base: '/page-code-generator/',
   plugins: [react()],
 });
