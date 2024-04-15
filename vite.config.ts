f59e8e3d48534f804e4c9144b8c30eece19ab579
zhanghaojie <1455123087@qq.com>
2024-04-15 13:51:38 +0800

    Add vite-tsconfig-paths plugin to package.json and tsconfig.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // for github pages
   base: '/page-code-generator/',
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 });
