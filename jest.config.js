b0dd61b68a2d29ff1add69e98e3f4bd203372774
zhanghaojie <1455123087@qq.com>
2024-07-01 15:07:05 +0800

    chore: Replace jest with vitest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('jest').Config} */
-export default {
-  testEnvironment: 'jsdom',
-  transformIgnorePatterns: [
-    '<rootDir>/bower_components/',
-    '<rootDir>/node_modules/',
-  ],
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // for github pages
-  base: '/page-code-generator/',
+  base: "/page-code-generator/",
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    clearMocks: true,
+    environment: "happy-dom",
+  },
 });
