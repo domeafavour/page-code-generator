9503276bd48a62c7d8bedc4b3f67d3b5fc948980
zhanghaojie <1455123087@qq.com>
2024-07-02 11:33:31 +0800

    refactor(Flex): Update Flex component to use SelectContent, SelectTrigger, and SelectValue from ui/select

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -2,19 +2,19 @@ import { cn } from '@/lib/utils';
 import { PropEditorProps } from '@/typings';
 import React from 'react';
 import { TextPropEditor } from './TextPropEditor';
-import { Select, SelectItem } from './ui/select';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
 
 interface ConfigurableProps {
-  flexDirection?: React.CSSProperties['flexDirection'];
+  flexDirection?: React.CSSProperties["flexDirection"];
   gap?: number;
-  alignItems?: React.CSSProperties['alignItems'];
+  alignItems?: React.CSSProperties["alignItems"];
 }
 
-type Props = ConfigurableProps & React.ComponentPropsWithoutRef<'div'>;
+type Props = ConfigurableProps & React.ComponentPropsWithoutRef<"div">;
 
 export type { ConfigurableProps as FlexConfigurableProps, Props as FlexProps };
 
-export const Flex = React.forwardRef<React.ComponentRef<'div'>, Props>(
+export const Flex = React.forwardRef<React.ComponentRef<"div">, Props>(
   (
     {
       gap = 8,
@@ -30,7 +30,7 @@ export const Flex = React.forwardRef<React.ComponentRef<'div'>, Props>(
     return (
       <div
         ref={forwardedRef}
-        className={cn('flex', className)}
+        className={cn("flex", className)}
         style={{ gap, alignItems, flexDirection, ...style }}
         {...props}
       >
@@ -41,17 +41,22 @@ export const Flex = React.forwardRef<React.ComponentRef<'div'>, Props>(
 );
 
 export function FlexDirectionEditor(
-  props: PropEditorProps<ConfigurableProps['flexDirection']>
+  props: PropEditorProps<ConfigurableProps["flexDirection"]>
 ) {
   return (
     <Select
       value={props.value}
       onValueChange={props.onChange as (value: string) => void}
     >
-      <SelectItem value="row">Row</SelectItem>
-      <SelectItem value="column">Column</SelectItem>
-      <SelectItem value="row-reverse">Row Reverse</SelectItem>
-      <SelectItem value="column-reverse">Column Reverse</SelectItem>
+      <SelectTrigger>
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="row">Row</SelectItem>
+        <SelectItem value="column">Column</SelectItem>
+        <SelectItem value="row-reverse">Row Reverse</SelectItem>
+        <SelectItem value="column-reverse">Column Reverse</SelectItem>
+      </SelectContent>
     </Select>
   );
 }
