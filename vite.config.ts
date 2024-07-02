34e71b1d5ed7971b45acfe6efcd0e6f706f6d922
zhanghaojie <1455123087@qq.com>
2024-07-02 22:12:30 +0800

    feat: Add vite-plugin-svgr for SVG support in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // for github pages
   base: "/page-code-generator/",
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr({
+      include: /\.svg$/,
+    }),
+  ],
   test: {
     globals: true,
     clearMocks: true,
