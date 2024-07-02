e91c5a6ea69747cc17e69b806c46cd07b9f7aaa8
zhanghaojie <1455123087@qq.com>
2024-07-02 14:20:59 +0800

    refactor: Update Toolbar component to use Flex and Button components for improved styling and functionality

--- src/containers/Toolbar.tsx
+++ src/containers/Toolbar.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import { Flex } from "@/components/Flex";
+import { Button } from "@/components/ui/button";
+import { HammerIcon } from "lucide-react";
 
-interface Props {}
+export function Toolbar() {
+  function generate() {}
 
-export type { Props as ToolbarProps };
-
-export function Toolbar(props: Props) {
-  return <div>Toolbar</div>;
+  return (
+    <Flex flexDirection="row" gap={8}>
+      <Button size="icon" onClick={generate}>
+        <HammerIcon />
+      </Button>
+    </Flex>
+  );
 }
